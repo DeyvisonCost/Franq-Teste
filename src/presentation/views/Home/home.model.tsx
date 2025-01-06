259d9805a02090a7d34efa8baaa4e8fe34b50aae
deyvison <deyvisongtr@gmail.com>
2025-01-06 03:47:44 -0300

    ci: testando variaveis de ambiente na vercel

--- src/presentation/views/Home/home.model.tsx
+++ src/presentation/views/Home/home.model.tsx
@@ -1,16 +1,35 @@
 import { useCallback, useEffect } from 'react'
 
+import axios from 'axios'
+
 import { fetchAllStocks } from '@/infra/instances/fetchStocksInstance/fetchAllStocksInstance'
+import { getEnv } from '@/utils'
 
 export const useHomeModel = () => {
   const handleAlert = () => {
     alert('Home')
   }
 
+  const githubTest = async () => {
+    const url = getEnv('VITE_GITHUB_URL')
+
+    try {
+      const { data } = await axios.get(`${url}/users`)
+      // eslint-disable-next-line no-console
+      console.log('data ', data)
+    } catch (err) {}
+  }
+
   const getApi = useCallback(async () => {
     try {
-      await fetchAllStocks()
+      const res = await fetchAllStocks()
+      // eslint-disable-next-line no-console
+      console.log('SDGFDSFG ', res)
     } catch (err) {}
+  }, [])
+
+  useEffect(() => {
+    githubTest()
   }, [])
 
   useEffect(() => {
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
-import { defineConfig } from 'vite' 
+import { defineConfig } from 'vite'
 
-import react from '@vitejs/plugin-react' 
-
-
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
@@ -11,13 +9,19 @@ export default defineConfig({
       '@': '/src',
     },
   },
-  
+
   server: {
     proxy: {
       '/api': {
         target: 'https://api.hgbrasil.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+
+      '/github': {
+        target: 'https://api.github.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/github/, ''),
       },
     },
   },
